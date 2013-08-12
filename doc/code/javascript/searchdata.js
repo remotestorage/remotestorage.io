111c79158995006ccdbb849a36e3647e30b30831
Michiel de Jong <michiel@unhosted.org>
2013-08-12 16:22:55 +0200

    add http://remotestoragejs.com/doc/... to http://remotestorage.io/doc/...

---
+++ doc/code/javascript/searchdata.js
@@ -0,0 +1,32 @@
+var indexSectionsWithContent = {
+   "General": {
+      "Symbols": false,
+      "Numbers": false,
+      "A": true,
+      "B": true,
+      "C": true,
+      "D": true,
+      "E": true,
+      "F": true,
+      "G": true,
+      "H": true,
+      "I": true,
+      "J": true,
+      "K": false,
+      "L": true,
+      "M": true,
+      "N": true,
+      "O": true,
+      "P": true,
+      "Q": false,
+      "R": true,
+      "S": true,
+      "T": true,
+      "U": true,
+      "V": true,
+      "W": true,
+      "X": false,
+      "Y": false,
+      "Z": false
+      }
+   }
