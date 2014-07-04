b4b40539fe392785de958b89408043e3a24fa89a
Sebastian Kippe <sebastian@kip.pe>
2014-07-04 15:29:17 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-git pull --rebase origin responsive_revamp
+git pull --rebase origin relaunch
 rm -rf _site/*
 jekyll build
 git add --all
 git commit -m "Update build - `date -u`"
 git subtree push --prefix _site 5apps master
+git push origin relaunch
